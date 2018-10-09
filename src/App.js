79ff296f2bd2db40e7af2a7f4d7fee4f53a09868
juliendubrail <dubrail.julien@gmail.com>
2018-10-09 22:17:36 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor(props){
+        super(props);
+        this.state = {
+            quote: 'Chuck Quote'
+        }
+    }
+
+    changeQuote(){
+        this.setState({quote:'Chuck Rules'})
+    }
+
+    render(){
+        return (
+            <div className="App">
+                <div className="App-title">{this.state.quote}</div>
+                <div>
+                    <button className="button" onClick={()=>this.changeQuote()}>Yay button</button>
+                    <button className="button">Nay button</button>
+                </div>
+                App Component
+            </div>
+        )
+    }
 }
 
 export default App;
