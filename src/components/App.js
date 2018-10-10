2e7323847ea5070c2a3f2d91d72b7c4c9eb679b5
juliendubrail <dubrail.julien@gmail.com>
2018-10-10 23:14:19 +0200

    added table

---
+++ src/components/App.js
@@ -0,0 +1,96 @@
+import React, { Component } from 'react';
+import Table from './table';
+import './App.css';
+
+class App extends Component {
+  // si tu n'utilise pas le constructor pour autre chose que pour set le initial state tu peux
+  // simplement faire ca:
+  state = {
+    quote: 'Chuck Quote',
+    // {
+    //     id:1,
+    //     abbvTitle:Good,
+    //     items:[
+    //         {quote:'Chuckk'}
+    //     ],
+    //     clicked: true
+    // },
+    // {
+    //     id:2,
+    //     abbvTitle:Bad,
+    //     items:[
+    //         {quote: null}
+    //     ],
+    //     clicked:false
+    // },
+    // {
+    //     id:3,
+    //     abbvTitle:All,
+    //     items:[
+    //         {quote:null}
+    //     ]
+    // }
+};
+
+  changeQuote = () => {
+    this.setState({ quote: 'Chuck Rules' });
+  };
+
+  searchQuote = () => {
+        const FETCH_URL = 'http://api.icndb.com/jokes/random';
+        fetch(FETCH_URL, {
+            method: 'GET'
+        })
+        .then(response => response.json())
+        .then(json => {
+        const quote = json.value.joke;
+        console.log('joke', quote);
+        this.setState({quote})
+        } )
+        .catch(error =>console.log(error))
+   };
+
+   getClickedButton = () => {
+       return this.state.find((button) => button.clicked)
+   }
+
+   // setClickedButtom = (buttonId) => {
+   //     this.state.map((buttonObj) => {
+   //         buttonObj.id === buttonId  ? buttonObj.clicked = true : buttonObj.clicked = false;
+   //     });
+   // }
+
+   submitQuote = (quote) => {
+       const clickedButton = this.getClickedButton();
+       clickedButton.items.push(quote)
+   }
+
+  render() {
+    // c'est bien de prendre l'habite de desctucturer, meme si la c'est overkill
+    const { quote } = this.state;
+
+    return (
+      <div className="App">
+        <div className="App-title">{quote}</div>
+        <div>
+          {/*
+        il vaut mieux eviter le plus possible de definir des fonctions dans le jsx. Tu peux check les
+        raisons dans ici: https://stackoverflow.com/questions/36677733/why-shouldnt-jsx-props-use-arrow-functions-or-bind
+        Du coup pour le onClick event handler, on peu referencer la App class method "changeQuote" directement au lieu de crer une fonction qui call changeQuote.
+        L'event rest disponible comme premier argument de this.changeQuote si t'en as besoin.
+        */}
+          <button className="button" onClick={this.submitQuote}>
+            Yay button
+          </button>
+           <button className="button" onClick={this.searchQuote}>
+               Nay button
+           </button>
+        </div>
+        <Table />
+        App Component
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/table.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Table extends Component {
+    render() {
+        return(
+            <div className="tablecontainer">
+                <div className="column">
+                    <h4>Good Jokes</h4>
+                    <p className="table_quote">Chuck Norris doesn't use GUI, he prefers COMMAND line.</p>
+                </div>
+                <div className="column">
+                    <h4>Bad Jokes</h4>
+                </div>
+                Table Component
+            </div>
+        )
+    }
+}
+
+export default Table;
